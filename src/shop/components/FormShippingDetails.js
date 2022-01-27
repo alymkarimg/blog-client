7457a967f8c9988edce3f357348c7c574dedad5d
Alym Karim <alymkarimg@gmail.com>
2022-01-27 11:07:52 +0000

    tester

--- src/shop/components/FormShippingDetails.js
+++ src/shop/components/FormShippingDetails.js
@@ -7,13 +7,55 @@ import Button from "@material-ui/core/Button";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import CheckoutForm from "./CheckoutForm";
+import { isPostCode } from "../../helpers/Default";
 
 const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY);
 
 export class FormCardDetails extends Component {
+  state = {
+    errors: {},
+  };
+
   continue = (e) => {
     e.preventDefault();
-    this.props.nextStep();
+    if (
+      this.props.values.address.length == 0 ||
+      this.props.values.city.length == 0 ||
+      !isPostCode(this.props.values.postcode) ||
+      this.props.values.country.length == 0
+    ) {
+      let newErrorsObject = {};
+      if (this.props.values.address.length == 0) {
+        newErrorsObject = {
+          ...newErrorsObject,
+          address: "please enter valid address",
+        };
+      }
+      if (this.props.values.city.length == 0) {
+        newErrorsObject = {
+          ...newErrorsObject,
+          city: "please enter valid city",
+        };
+      }
+      if (!isPostCode(this.props.values.postcode)) {
+        newErrorsObject = {
+          ...newErrorsObject,
+          postcode: "please enter valid postcode",
+        };
+      }
+      if (this.props.values.country.length == 0) {
+        newErrorsObject = {
+          ...newErrorsObject,
+          country: "please enter valid country",
+        };
+      }
+      this.setState({
+        ...this.state,
+        errors: newErrorsObject
+      })
+    } else {
+      this.props.nextStep();
+    }
   };
 
   back = (e) => {
@@ -37,6 +79,8 @@ export class FormCardDetails extends Component {
               <TextField
                 placeholder="Enter Your address"
                 label="Address"
+                error={!!this.state.errors.address}
+                helperText={this.state.errors.address}
                 onChange={handleChange("address")}
                 defaultValue={values.address}
                 margin="normal"
@@ -46,6 +90,8 @@ export class FormCardDetails extends Component {
               <TextField
                 placeholder="Enter Your city"
                 label="City"
+                error={!!this.state.errors.city}
+                helperText={this.state.errors.city}
                 onChange={handleChange("city")}
                 defaultValue={values.city}
                 margin="normal"
@@ -55,6 +101,8 @@ export class FormCardDetails extends Component {
               <TextField
                 placeholder="Enter Your Postcode"
                 label="Postcode"
+                error={!!this.state.errors.postcode}
+                helperText={this.state.errors.postcode}
                 onChange={handleChange("postcode")}
                 defaultValue={values.postcode}
                 margin="normal"
@@ -63,6 +111,8 @@ export class FormCardDetails extends Component {
               <TextField
                 placeholder="Enter Your Country"
                 label="Country"
+                error={!!this.state.errors.country}
+                helperText={this.state.errors.country}
                 onChange={handleChange("country")}
                 defaultValue={values.country}
                 margin="normal"
